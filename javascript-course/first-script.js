9c85250168674a6125f935dae4c4e955cb3fd559
Cryxto <bagasadvan123@gmail.com>
2022-03-16 02:09:48 +0700

    first touch with javascript

---
+++ javascript-course/first-script.js
@@ -0,0 +1,8 @@
+console.log("Hello there");
+let input = prompt("Welcome to Holy Crown Bar, could you please enter your age?");
+input = parseInt(input);
+if (input>=18){
+    alert("Enjoy !!");
+}else{
+    alert("Sorry, you are underage! Come when you are old enough!!");
+}
